f6c1c1b779e2f61606ed734bcd589f6c15afc2bc
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-05-25 23:47:26 -0300

    Update OrientacoesController.java

--- projeto/src/main/java/br/com/wegone/projeto/controlller/OrientacoesController.java
+++ projeto/src/main/java/br/com/wegone/projeto/controlller/OrientacoesController.java
@@ -5,8 +5,11 @@ import java.util.*;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +34,19 @@ public class OrientacoesController{
 		orientPt newOrientPt = dao.save(orientpt);
 		return newOrientPt;
 	}
-
+	
+	@PutMapping 
+	public orientPt editaOrientPt (@RequestBody orientPt orientpt) {
+		orientPt updateOrientPt = dao.save(orientpt);
+		return updateOrientPt;
+	}
+	
+	@DeleteMapping("/{id}")
+	public Optional<orientPt> deleteOrientPt(@PathVariable Integer id) {
+		Optional<orientPt> orientpt = dao.findById(id);
+		dao.deleteById(id);
+		return orientpt;
+	}
+	
+	
 }
